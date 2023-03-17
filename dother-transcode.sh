2135b2a88349c602f950da214af0fe56ecb4a941
Martin Pickett <martinpickett8@googlemail.com>
2023-03-17 18:29:22 +0000

    Added support for multiple file inputs. This actually splits them up and runs each in sucession, but the same result is achieved.

--- dother-transcode.sh
+++ dother-transcode.sh
@@ -1,31 +1,42 @@
 #!/usr/bin/env bash
 
-# Loop through input parameters looking for "--help" and "--version"
+# Initiate 'noEncode' flag and empty arrays for files and non-file parametes
 noEncode=false
+files=()
+newparams=()
+
+# Check if there are any parameters, if not set 'noEncode' flag
 if [ $# -eq 0 ]; then
     noEncode=true
 else
+    # Loop through all input parameters
     for param in "$@"; do
+        # Set 'noEncode' flag if '--help', '-h' or '--version' appear in parameters
 	    if [ "$param" = "--help" ] || [ "$param" = "-h" ] || [ "$param" = "--version" ]; then
 		    noEncode=true
+        fi
+
+        # Seperate files from the rest of the parameters
+        if [ -f "$param" ]; then
+            files+=("$param")
+        else
+            newparams+=("$param")
         fi
     done
 fi
 
+# If 'noEncode' flag has been set, run simplified docker command
 if [ "$noEncode" = true ]; then
     exec docker run --rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" ghcr.io/ttys0/other-transcode:sw-latest "$@"
 else
-    # Number of arguments minus 1
-    num_args=$(($#-1))
+    # Loop throught each file in files
+    for file in "${files[@]}"; do
+        # Generate absolute file path and split into diretory and filename
+        absolute_input=$(realpath "$file")
+        directory=$(dirname "$absolute_input")
+        filename=$(basename "$absolute_input")
 
-    # input file path (assuming last argument is the input file)
-    input_file_path="${@:$#}"
-
-    # Generate absolute file path and split into diretory and filename
-    absolute_input=$(realpath "$input_file_path")
-    directory=$(dirname "$absolute_input")
-    filename=$(basename "$absolute_input")
-
-    # docker command
-    exec docker run --rm -v "$directory":/src -v "$(pwd)":/out -w /out ghcr.io/ttys0/other-transcode:sw-latest "${@:1:$num_args}" "/src/$filename"
+        # docker command
+        docker run --rm -v "$directory":/src -v "$(pwd)":/out -w /out ghcr.io/ttys0/other-transcode:sw-latest "${newparams[@]}" "/src/$filename"
+    done
 fi
